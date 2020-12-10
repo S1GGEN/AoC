e8f33ddbe8558bf45ea6a1ded27b64618ed1156a
Sigurd Øines <sigurdoines@gmail.com>
2020-12-10 20:02:43 +0100

    Some work on day7

--- AoC_2020/python/benchmark.py
+++ AoC_2020/python/benchmark.py
@@ -1,23 +1,26 @@
 
 
-
-def timereps(reps, func, argument):
+def timereps(reps, func, arg):
     from time import time
     start = time()
     for i in range(0, reps):
-        func(argument)
+        func(arg)
     end = time()
     return "{:.10f}".format((end - start) / reps)
 
 
 def d07():
-    from day7.day7 import one as d07p1, two as d07p2
+    from day7.day7 import one as d07p1, two as d07p2, two_old as d07p2_old
     print("\nBENCHMARK DAY 07:")
     number = 1000
-    day7_input = open('day7/input.txt', 'r')
-    # print(f"opened input {number} times, average result: {timereps(number, open('day7/input.txt', 'r'))} seconds")
-    print(f"Ran part1() {number} times, average result: {timereps(number, d07p1, day7_input)} seconds")
-    print(f"Ran part2() {number} times, average result: {timereps(number, d07p2, day7_input)} seconds")
+    # with open('day7/input.txt', 'r') as f:
+        # day7_input = open('day7/input.txt', 'r')
+        # for line in day7_input:
+        #     print(line)
+        # print(f"opened input {number} times, average result: {timereps(number, open('day7/input.txt', 'r'))} seconds")
+        # print(f"Ran part1() {number} times, average result: {timereps(number, d07p1, f)} seconds")
+        # print(f"Ran part2() {number} times, average result: {timereps(number, d07p2, f)} seconds")
+        # print(f"Ran part2() old version {number} times, average result: {timereps(number, d07p2_old, f)} seconds")
 
 
 d07()
--- AoC_2020/python/day7/day7.py
+++ AoC_2020/python/day7/day7.py
@@ -1,10 +1,14 @@
 import networkx as nx
 # import matplotlib.pyplot as plt
 
-def one(task_input):
+
+def one():
+    task_input = open('day7/input.txt', 'r')
     graph = make_graph(task_input)
+    # print(len(graph.nodes))
     in_edges = get_in_edges(graph, "shiny_gold")
     return len(set(in_edges))
+
 
 def get_in_edges(graph, node):
     results = []
@@ -15,24 +19,48 @@ def get_in_edges(graph, node):
         results += get_in_edges(graph, in_edge)
     return results
 
-def two(task_input):
+
+def two():
+    task_input = open('day7/input.txt', 'r')
     graph = make_graph(task_input)
-    two_sum = get_out_sum(graph, "shiny_gold", 1)
+    # print(len(graph.nodes))
+    return get_out_sum(graph, "shiny_gold")
 
-    return two_sum
 
-def get_out_sum(graph, node, multiplier):
+def two_old():
+    task_input = open('day7/input.txt', 'r')
+    graph = make_graph(task_input)
+    # print(len(graph.nodes))
+    return get_out_sum_old(graph, "shiny_gold")
+
+def get_out_sum(graph, node):
+    # No need to calculate if we previously crawled from this node
+    if node in graph.nodes:
+        if 'sub_bags_sum' in graph.nodes[node]:
+            return graph.nodes[node]['sub_bags_sum']
+
     out_edge_sum = 0
     out_edges = graph.out_edges([node])
     for edge in out_edges:
         _, out_node = edge
         weight = int(graph.edges[node, out_node]['weight'])
-        out_edge_sum += int(weight) * int(multiplier)
-        out_edge_sum += get_out_sum(graph, out_node, weight * multiplier)
+        out_edge_sum += int(weight)
+        out_edge_sum += weight * get_out_sum(graph, out_node)
     # Store this value in the node, so that we can access it directly later:
-    # graph.nodes[node]['sub_bags'] = x
-    # https://networkx.org/documentation/stable/tutorial.html#node-attributes
+    graph.nodes[node]['sub_bags_sum'] = out_edge_sum
     return out_edge_sum
+
+
+def get_out_sum_old(graph, node):
+    out_edge_sum = 0
+    out_edges = graph.out_edges([node])
+    for edge in out_edges:
+        _, out_node = edge
+        weight = int(graph.edges[node, out_node]['weight'])
+        out_edge_sum += int(weight)
+        out_edge_sum += weight * get_out_sum_old(graph, out_node)
+    return out_edge_sum
+
 
 def make_graph(task_input):
     graph = nx.DiGraph()
@@ -57,10 +85,9 @@ def make_graph(task_input):
 
     return graph
 
-def main():
-    input = open('day7/input.txt', 'r')
-    print("Day 7 part 1:", one(input))
-    print("Day 7 part 2:", two(input))
 
+if __name__ == "__main__":
+    # input = open('day7/input.txt', 'r')
+    print("Day 7 part 1:", one())
+    print("Day 7 part 2:", two())
 
-main()
