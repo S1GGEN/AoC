f37772c2e673f01c82c71e0eca685d57b2ff065a
Sigurd Øines <sigurdoines@gmail.com>
2020-12-13 16:27:26 +0100

    Day 13

---
+++ AoC_2020/rust_aoc2020/benches/day13.rs
@@ -0,0 +1,12 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use rust_aoc2020::days::day13::*;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let input = load_input("input");
+    c.bench_function("day 13 load input", |b| b.iter(|| load_input("input")));
+    c.bench_function("day 13 part one", |b| b.iter(|| one(&input)));
+    c.bench_function("day 13 part two ", |b| b.iter(|| two(&input)));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ AoC_2020/rust_aoc2020/src/days/day13/mod.rs
@@ -0,0 +1,83 @@
+use std::fs;
+use std::str;
+
+pub fn load_input(filename : &str) -> String{
+    let input = fs::read_to_string(format!("src/days/day13/{}.txt", filename))
+        .expect("Something went wrong reading the file");
+
+    return input;
+}
+
+pub fn one(input : &str) -> String {
+    let result = calculate_one(input);
+
+    match result {
+        Some(r) => format!("Task 1: {}", r),
+        None => "Task 1: FAILED".to_string(),
+    }
+}
+
+
+fn calculate_one(input : &str) -> Option<u32> {
+    let mut lines = input.lines();
+
+    let earliest_time = to_int(lines.next().unwrap());
+    let bus_ids : Vec<u32> = lines.next().unwrap().split(",").filter(|x| x != &"x").map(|x| to_int(x)).collect();
+
+
+    let mut earliest_bus_timestamp = u32::MAX;
+    let mut earliest_bus_id= None;
+
+    for bus_id in bus_ids {
+        let timestamp = ((earliest_time as f32/bus_id as f32).ceil() as u32 * bus_id) as u32;
+        if timestamp < earliest_bus_timestamp {
+            earliest_bus_timestamp = timestamp;
+            earliest_bus_id = Some(bus_id);
+        }
+    }
+
+    Some((earliest_bus_timestamp - earliest_time) * earliest_bus_id.unwrap())
+}
+
+pub fn two(input : &str) -> String {
+    let result = calculate_two(input);
+
+    match result {
+        Some(r) => format!("Task 2: {}", r),
+        None => "Task 2: FAILED".to_string(),
+    }
+}
+
+
+fn calculate_two(input : &str) -> Option<u64> {
+    let bus_ids : Vec<&str> = input.lines().nth(1).unwrap().split(",").collect();
+
+    let mut bus_offset = 0;
+
+    let mut busses : Vec<(u64, u64)> = vec![]; // (bus_id, offset)
+
+    for bus_id in &bus_ids {
+        if bus_id != &"x" {
+            busses.push((to_int(bus_id) as u64, bus_offset))
+        }
+        bus_offset += 1;
+    }
+
+    let mut departure_timestamp = 0;
+    let mut advancement = 1;
+
+    for bus in &busses {
+        let (bus_id, bus_offset) = bus;
+        while (departure_timestamp + bus_offset) % bus_id != 0 {
+            departure_timestamp += advancement
+        }
+        advancement *= bus_id
+    }
+
+    Some(departure_timestamp)
+}
+
+fn to_int(string : &str) -> u32 {
+    return string.parse::<u32>().unwrap();
+}
+
